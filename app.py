a352857a513b3a72f7b32f6b3afa74c1bec4c527
ff-yifei <1072923037@qq.com>
2025-06-26 22:36:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 st.title("图片选择展示网站")
 
 # 左侧参数选择
-feature1 = st.sidebar.selectbox("Gene", ["ACTA2","CD3D", "CD3E","CD4","CD8A", "CD14","CD68","CD79A","CLEC10A","COL1A1","CSF3R","DCN","FAP","FOXP3","IGHG1","IGKC","JCHAIN","KRT8","KRT18","KRT19","NKG7","TPSB2","VWF","EPCAM"])
+feature1 = st.sidebar.selectbox("Gene", ["CD3D", "CD3E","CD4","CD8A", "CD14","CD68","CD79A","CLEC10A","COL1A1","CSF3R","DCN","FAP","FOXP3","IGHG1","IGKC","JCHAIN","KRT8","KRT18","KRT19","NKG7","TPSB2","VWF","EPCAM"])
 feature2=st.sidebar.selectbox("Major cell type", ["ACTA2"])
 submit = st.sidebar.button("提交")
 
@@ -17,3 +17,9 @@ if submit:
     else:
         st.warning("找不到对应的图片，请确认参数组合和文件名是否一致。")
 
+if submit:
+    image_path = f"images/{feature2}.png"
+    if os.path.exists(image_path):
+        st.image(Image.open(image_path), caption=f"{feature1}", use_container_width=True)
+    else:
+        st.warning("找不到对应的图片，请确认参数组合和文件名是否一致。")
