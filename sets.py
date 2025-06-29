100c869ae0e9207640e33778186fe8bc6c500eda
ff-yifei <1072923037@qq.com>
2025-06-29 22:21:27 +0800

    Update sets.py

--- sets.py
+++ sets.py
@@ -10,9 +10,9 @@ with left_col:
 st.subheader("UMAP Plot")
 col1, col2 = st.columns(2)
 with col1:
-feature1 = st.sidebar.selectbox("Gene", ["ACTA2","CD3D", "CD3E","CD4","CD8A", "CD14","CD68","CD79A","CLEC10A","COL1A1","CSF3R","DCN","FAP","FOXP3","IGHG1","IGKC","JCHAIN","KRT8","KRT18","KRT19","NKG7","TPSB2","VWF","EPCAM"])
+ feature1 = st.sidebar.selectbox("Gene", ["ACTA2","CD3D", "CD3E","CD4","CD8A", "CD14","CD68","CD79A","CLEC10A","COL1A1","CSF3R","DCN","FAP","FOXP3","IGHG1","IGKC","JCHAIN","KRT8","KRT18","KRT19","NKG7","TPSB2","VWF","EPCAM"])
 with col2:
-feature2 = st.sidebar.selectbox("Major cell type",["ACTA2"])   
+ feature2 = st.sidebar.selectbox("Major cell type",["ACTA2"])   
 submit = st.sidebar.button("GO")
   
 st.markdown("---")
@@ -21,9 +21,9 @@ st.markdown("---")
 st.subheader("Violin Plot")
 col3, col4 = st.columns(2)
 with col3:
-feature3 = st.sidebar.selectbox("Gene",  ["ACTA2", "CD3D", "CD8A", "CD68", "EPCAM", "VWF"])
+ feature3 = st.sidebar.selectbox("Gene",  ["ACTA2", "CD3D", "CD8A", "CD68", "EPCAM", "VWF"])
 with col4:
-feature4 = st.sidebar.selectbox("meta information", ["ACTA2", "CD3D"])
+ feature4 = st.sidebar.selectbox("meta information", ["ACTA2", "CD3D"])
     
  if submit:
     image_path = f"images/{feature1}.png" or f"images/{feature2}.png"
