fd78fa20e6777430798c21609bf458382b676d89
ff-yifei <1072923037@qq.com>
2025-06-29 22:20:28 +0800

    Update sets.py

--- sets.py
+++ sets.py
@@ -8,20 +8,20 @@ with left_col:
     # 左侧区域分为上下两部分
    st.header("🛠️ 控制面板")
 st.subheader("UMAP Plot")
-    col1, col2 = st.columns(2)
+col1, col2 = st.columns(2)
 with col1:
-    feature1 = st.sidebar.selectbox("Gene", ["ACTA2","CD3D", "CD3E","CD4","CD8A", "CD14","CD68","CD79A","CLEC10A","COL1A1","CSF3R","DCN","FAP","FOXP3","IGHG1","IGKC","JCHAIN","KRT8","KRT18","KRT19","NKG7","TPSB2","VWF","EPCAM"])
+feature1 = st.sidebar.selectbox("Gene", ["ACTA2","CD3D", "CD3E","CD4","CD8A", "CD14","CD68","CD79A","CLEC10A","COL1A1","CSF3R","DCN","FAP","FOXP3","IGHG1","IGKC","JCHAIN","KRT8","KRT18","KRT19","NKG7","TPSB2","VWF","EPCAM"])
 with col2:
 feature2 = st.sidebar.selectbox("Major cell type",["ACTA2"])   
 submit = st.sidebar.button("GO")
   
-    st.markdown("---")
+st.markdown("---")
     
     # 下部：分析选项
-    st.subheader("Violin Plot")
+st.subheader("Violin Plot")
 col3, col4 = st.columns(2)
 with col3:
-    feature3 = st.sidebar.selectbox("Gene",  ["ACTA2", "CD3D", "CD8A", "CD68", "EPCAM", "VWF"])
+feature3 = st.sidebar.selectbox("Gene",  ["ACTA2", "CD3D", "CD8A", "CD68", "EPCAM", "VWF"])
 with col4:
 feature4 = st.sidebar.selectbox("meta information", ["ACTA2", "CD3D"])
     
