b39d7ffbe011270a27c2752fe9705fe97c34fc8e
ff-yifei <1072923037@qq.com>
2025-07-20 16:05:12 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,44 @@
-import streamlit as st
-from PIL import Image
 import os
+from flask import Flask, send_file, jsonify, request,render_template
+import pandas as pd
 
-st.title("图片选择展示网站")
-
-# 左侧参数选择
-feature1 = st.sidebar.selectbox("Gene", ["ACTA2","CD3D", "CD3E","CD4","CD8A", "CD14","CD68","CD79A","CLEC10A","COL1A1","CSF3R","DCN","FAP","FOXP3","IGHG1","IGKC","JCHAIN","KRT8","KRT18","KRT19","NKG7","TPSB2","VWF","EPCAM"])
-
-submit = st.sidebar.button("提交")
-
-# 显示图片
-if submit:
-    image_path = f"images/{feature1}.png"
-    if os.path.exists(image_path):
-        st.image(Image.open(image_path), caption=f"{feature1}", use_container_width=True)
+def search_third_column(csv_file, col1_value, col2_value):
+    # 读取CSV文件
+    df = pd.read_csv(csv_file)
+    
+    # 匹配前两列的值，获取对应的第三列数据
+    # 假设CSV的列名分别为'col1'、'col2'、'col3'，可根据实际列名修改
+    result = df[(df['Gene'] == col1_value) & (df['Meta information'] == col2_value)]['image_path']
+    
+    # 处理结果
+    if not result.empty:
+        return result.values[0]  # 返回匹配到的第三列值
     else:
-        st.warning("找不到对应的图片，请确认参数组合和文件名是否一致。")
+        return "未找到匹配的数据"
 
+app = Flask(__name__)
+# 配置 PDF 存储目录，这里使用当前目录下的 pdfs 文件夹
+PDF_FOLDER = os.path.join(os.getcwd(), 'static/umap_figure')
+os.makedirs(PDF_FOLDER, exist_ok=True)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/pdfs',methods={'POST'})
+def serve_pdf():
+    i3 = request.form.get('plotType')
+    if i3 == 'umap':
+        csv_path = "mapping.csv"  # 替换为你的CSV文件路径
+    else:
+        csv_path = "mapping-violin.csv" 
+    input_col1 = request.form.get('gene')
+    input_col2 = request.form.get('cellType')
+    #i3 = request.form.get('plotType')
+    print(input_col1,input_col2)
+    third_col_value = search_third_column(csv_path, input_col1, input_col2)
+    print(f"对应的第三列值为：{third_col_value}")
+    
+    return jsonify({'success':'success','type':i3,'pdfUrl':'static/'+third_col_value})
+    
+if __name__ == '__main__':
+    app.run(debug=True)
 
